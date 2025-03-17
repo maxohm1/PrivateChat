af01b4445485dc3241a96244460d0b1667228220
maxohm1 <maxohm24@gmail.com>
2025-03-17 20:31:05 +0530

    Home Screen completely done. logo, camera, search, more, profile photo, name, message, and time. and scaffold floating button, bottom navigation- chat, updates, communities, calls are perfectly features.

---
+++ app/src/main/java/max/ohm/privatechat/presentation/homescreen/ChatDesignModel.kt
@@ -0,0 +1,9 @@
+package max.ohm.privatechat.presentation.homescreen
+
+data class ChatDesignModel(
+    val image:Int,
+    val name: String,
+    val time :String,
+    val message: String
+
+)
--- app/src/main/java/max/ohm/privatechat/presentation/homescreen/HomeScreen.kt
+++ app/src/main/java/max/ohm/privatechat/presentation/homescreen/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -25,13 +26,84 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import androidx.compose.foundation.lazy.*
 import max.ohm.privatechat.R
+import max.ohm.privatechat.presentation.bottomnavigation.BottomNavigation
 
 @Composable
 @Preview(showSystemUi = true)
 
 fun HomeScreen() {
+
+
+  val chatData = listOf(
+      ChatDesignModel(
+       R.drawable.salman_khan,
+      name = "Salman Khan",
+      time= "10:00 AM",
+      message = "Hello om how are you"
+      ),
+
+      ChatDesignModel(
+          image = R.drawable.sharukh_khan,
+          name = "Sharukh Khan",
+          time= "12:00 AM",
+          message = "chal om ferrari me ghumte hai"
+      ),
+
+      ChatDesignModel(
+          image = R.drawable.akshay_kumar,
+          name = "Akshay Kumar",
+          time= "1:00 AM",
+          message = "my next movie shooting with you"
+      ),
+      ChatDesignModel(
+          image = R.drawable.disha_patani,
+          name = "Disha Patani",
+          time= "11:00 PM",
+          message = "please mere birthday me aa jana om"
+      ),
+
+      ChatDesignModel(
+          image = R.drawable.rashmika,
+          name = "Rashmika",
+          time= "10:44 AM",
+          message = "Come back to tollywood om"
+      ),
+      ChatDesignModel(
+          image = R.drawable.rajkummar_rao,
+          name = "Rajkumar Rai",
+          time= "12:50 AM",
+          message = "om,Ek new movie me shooting karoge"
+      ),
+      ChatDesignModel(
+          image = R.drawable.hrithik_roshan,
+          name = "Hrithik Roshan",
+          time= "5:46 AM",
+          message = "Om, you are main hero of krrish 4"
+      ),
+      ChatDesignModel(
+          image = R.drawable.ajay_devgn,
+          name = "Ajay Devgan",
+          time= "6:00 AM",
+          message = "Mumbai aaja teri bhut yaad aa rhi"
+      ),
+
+      ChatDesignModel(
+          image = R.drawable.bhuvan_bam,
+          name = "Bhuvam Bam",
+          time= "6:45 AM",
+          message = "chal aa ek webseries shooting karte hain"
+      ),
+      ChatDesignModel(
+          image = R.drawable.kartik_aaryan,
+          name = "Kartik Aaryan",
+          time= "6:36 AM",
+          message = "Do you favour on me om "
+      )
+
+
+      )
 
    // top bar and bottom bar foating bar addeded here
     Scaffold(
@@ -49,6 +121,10 @@ fun HomeScreen() {
 
             }
 
+        },
+
+        bottomBar = {
+            BottomNavigation()
         }
 
 
@@ -91,6 +167,14 @@ fun HomeScreen() {
 
             }
             HorizontalDivider()
+
+            LazyColumn {
+
+             items(chatData){
+
+                 ChatDesign(chatDesignModel = it )
+             }
+            }
 
 
         }
